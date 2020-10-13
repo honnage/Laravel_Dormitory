5bd97be0376679b5e2e2542c8da0fc7c178fb361
honnage <hon_da-1998@hotmail.com>
2020-10-13 18:11:26 +0700

    Conctroller test Simplex

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\DormitoryController;//เรียกแบบกำหนดfunction แบบที่ 1
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//|--------------------------------------------------------------------------
+/*
+    Route::get('/customer' , [DormitoryController::class,'index']); //เรียกแบบกำหนดfunction แบบที่ 1
+    Route::get('/custo', 'App\Http\Controllers\DormitoryController@index'); //เรียกแบบกำหนดfunction แบบที่ 2
+    Route::resource('c', 'DormitoryController');//เรียกแบบresource แบบเก่า ใช่ไม่ได้แล้ว
+    Route::resource('cus', 'App\Http\Controllers\DormitoryController');//เรียกแบบresource แบบใหม่
+*/
+//|--------------------------------------------------------------------------
+
+Route::resource('dormitory', 'App\Http\Controllers\DormitoryController');//เรียกแบบresource แบบใหม่
+
 Route::get('/', function () {
     // return view('welcome');
     return view('/auth/login');
@@ -22,6 +33,3 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
