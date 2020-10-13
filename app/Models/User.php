69f08777cb447238506695ae2812091ff070eea6
honnage <hon_da-1998@hotmail.com>
2020-10-13 18:25:16 +0700

    Validate Username

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,9 +17,10 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name',
+        // 'name',
         'email',
         'password',
+        'username',
     ];
 
     /**
